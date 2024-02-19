a5cac8e35234e64424368a94e9269f67b532c7da
PZahnen <124141114+PZahnen@users.noreply.github.com>
2024-02-19 16:15:51 +0100

    dynamic routes part 1

--- src/app/entities/[id]/page.tsx
+++ src/app/entities/[id]/page.tsx
@@ -3,13 +3,62 @@ import { Button } from "@/components/shadcn-ui/button";
 import { GetEntities } from "@/lib/utils";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { notFound } from "next/navigation";
-
-import { Tabs, TabsList, TabsTrigger } from "@/components/shadcn-ui/tabs";
-import { healthChecks } from "@/data/health";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "@/components/shadcn-ui/tabs";
+import { getHealthChecks } from "@/lib/utils";
 import Summary from "@/components/dashboard/summary";
+import { useState, useEffect } from "react";
+import { Entity } from "@/data/entities";
+import { Check } from "@/data/health";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id }: any = context.params;
+
+  return {
+    props: {
+      params: {
+        id: id,
+      },
+    },
+  };
+};
 
 export default function CustomerPage({ params }: { params: { id: string } }) {
-  const entities = GetEntities();
+  const [entities, setEntities] = useState<Entity[]>([]);
+  const [healthChecks, setHealthChecks] = useState<Check[]>([]);
+  const [tab, setTab] = useState("health");
+
+  const loadHealthChecks = async () => {
+    try {
+      const newHealthChecks = await getHealthChecks();
+      setHealthChecks(newHealthChecks);
+    } catch (error) {
+      console.error("Error loading health checks:", error);
+    }
+  };
+
+  const loadEntities = async () => {
+    try {
+      const newEntities = await GetEntities();
+      setEntities(newEntities);
+    } catch (error) {
+      console.error("Error loading entities:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadEntities();
+    loadHealthChecks();
+  }, []);
+
+  const onTabChange = (tab: string) => {
+    setTab(tab);
+  };
   const entity = entities.find((e) => (e.uid = params.id));
 
   if (!entity) {
@@ -21,40 +70,61 @@ export default function CustomerPage({ params }: { params: { id: string } }) {
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">{entity.id}</h2>
         <div className="flex items-center space-x-2">
-          <Button>
+          <Button
+            onClick={() => {
+              loadEntities();
+              loadHealthChecks();
+            }}
+          >
             <ReloadIcon className="mr-2 h-4 w-4" />
             Reload
           </Button>
         </div>
       </div>
 
-      <Tabs defaultValue="health" className="h-full space-y-6">
+      <Tabs
+        value={tab}
+        onValueChange={onTabChange}
+        className="h-full space-y-6"
+      >
         <div className="space-between flex items-center">
           <TabsList>
             <TabsTrigger value="health">
               <span>Health</span>
+            </TabsTrigger>
+            <TabsTrigger value="store">
+              <span>Store</span>
             </TabsTrigger>
             <TabsTrigger value="cfg">
               <span>Configuration</span>
             </TabsTrigger>
           </TabsList>
         </div>
+
+        <TabsContent value="health">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            {healthChecks
+              .filter(
+                (check) =>
+                  check.name.startsWith("db") && check.name.includes(entity.id)
+              )
+              .map((check) => (
+                <Summary
+                  key={check.name}
+                  header={`${check.healthy}`}
+                  main={check.name}
+                  footer={`${check.timestamp}`}
+                />
+              ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="store">
+          <div>Store content goes here</div>
+        </TabsContent>
+        <TabsContent value="cfg">
+          <div>Configuration content goes here</div>
+        </TabsContent>
       </Tabs>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {healthChecks
-          .filter(
-            (check) =>
-              check.name.startsWith("db") && check.name.includes(entity.id)
-          )
-          .map((check) => (
-            <Summary
-              key={check.name}
-              header={`${check.healthy}`}
-              main={check.name}
-              footer={`${check.timestamp}`}
-            />
-          ))}
-      </div>
     </div>
   );
 }
