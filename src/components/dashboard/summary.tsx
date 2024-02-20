7f1c002416fd70c96edb2ca621cacbc4f1b2e9b7
PZahnen <124141114+PZahnen@users.noreply.github.com>
2024-02-20 13:07:47 +0100

    changing style of cards

--- src/components/dashboard/summary.tsx
+++ src/components/dashboard/summary.tsx
@@ -25,25 +25,25 @@ export default function CustomersPage({
   Icon,
 }: SummaryProps) {
   return (
-    <Card className="shadow-lg">
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle
-          className={`text-sm font-semibold ${
-            header === "ACTIVE"
-              ? "text-blue-700 hover:text-blue-500"
-              : "text-red-700 hover:text-red-500"
-          }`}
-        >
-          {header}
-        </CardTitle>
-        {Icon ? <Icon className="h-4 w-4 text-muted-foreground" /> : null}
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold ">
-          {route ? <Link href={route}>{main}</Link> : main}
-        </div>
-        <p className="text-xs text-muted-foreground">{footer}</p>
-      </CardContent>
-    </Card>
+    <Link href={route || "#"}>
+      <Card className="shadow-lg hover:bg-gray-100 transition-colors duration-200">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle
+            className={`text-sm font-semibold ${
+              header === "ACTIVE"
+                ? "text-blue-700 hover:text-blue-500"
+                : "text-red-700 hover:text-red-500"
+            }`}
+          >
+            {header}
+          </CardTitle>
+          {Icon ? <Icon className="h-4 w-4 text-muted-foreground" /> : null}
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold ">{main}</div>
+          <p className="text-xs text-muted-foreground">{footer}</p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
