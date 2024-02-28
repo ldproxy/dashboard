e8e06c2d0440955b6d2296d43fdbe3fd06da0464
PZahnen <124141114+PZahnen@users.noreply.github.com>
2024-02-28 14:15:51 +0100

    redirect to "/deployment" on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/shadcn-ui/button";
 import { ReloadIcon } from "@radix-ui/react-icons";
 
 export default function ValuesPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/deployment");
+  }, []);
+
   return (
     <div className="flex-1 space-y-4 p-8 pt-0">
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">Overview</h2>
-      </div>
-      <div>TODO</div>
+      <div className="flex items-center justify-between space-y-2"></div>
     </div>
   );
 }
