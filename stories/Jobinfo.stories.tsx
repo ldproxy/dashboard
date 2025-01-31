7001853a0bc763499132cfd8899b7311b6c407d8
p.zahnen <pascal.zahnen@gmx.de>
2025-01-31 18:25:50 +0100

    update storybook jobs

--- stories/Jobinfo.stories.tsx
+++ stories/Jobinfo.stories.tsx
@@ -1,81 +1,46 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import CustomersPage, {
-  SummaryProps,
-} from "../src/components/dashboard/Jobinfo";
+import JobInfo, { SummaryProps } from "../src/components/dashboard/Jobinfo";
 import StoryWrapper from "./StoryWrapper";
+import { fromDev } from "../src/dev-data/jobs";
 
 export default {
   title: "Components/JobInfo",
-  component: CustomersPage,
+  component: JobInfo,
 } as Meta;
 
 const Template: StoryFn<SummaryProps> = (args) => (
   <StoryWrapper>
-    <CustomersPage {...args} />
+    <JobInfo {...args} />
   </StoryWrapper>
 );
+
+// Extrahieren der Daten aus fromDev
+const devData = fromDev();
+const exampleJob = devData[0].response.sets[0];
+
 export const Default = Template.bind({});
 Default.args = {
-  entity: "verwaltungseinheit",
-  tilesets: {
-    WebMercatorQuad: {
-      progress: {
-        percent: 77,
-        levels: {
-          0: [5],
-          1: [5],
-          2: [4],
-          3: [5],
-          4: [4],
-          5: [45],
-          6: [100],
-          7: [77],
-          8: [8],
-          9: [9],
-          10: [10],
-          11: [11],
-          12: [12],
-          13: [14],
-          14: [14],
-          15: [15],
-          16: [16],
-          17: [17],
-          18: [18],
-          19: [19],
-          20: [20],
-          21: [21],
-          22: [22],
-          23: [23],
-        },
-      },
-    },
-  },
-  label: "Example Job",
-  percent: 77,
-  startedAt: 1620000000,
-  updatedAt: 1620001000,
-  id: "job-1",
+  entity: exampleJob.entity,
+  tilesets: exampleJob.details.tileSets,
+  label: exampleJob.label,
+  percent: exampleJob.percent,
+  startedAt: exampleJob.startedAt,
+  updatedAt: exampleJob.updatedAt,
+  id: exampleJob.id,
   info: "Processing tiles",
+  url: devData[0].url,
 };
 
 export const Ready = Template.bind({});
 Ready.args = {
-  entity: "verwaltungseinheit",
-  tilesets: {
-    WebMercatorQuad: {
-      progress: {
-        percent: 100,
-        levels: {
-          1: [100],
-        },
-      },
-    },
-  },
-  label: "Example Job",
+  entity: exampleJob.entity,
+  tilesets: exampleJob.details.tileSets,
+  label: exampleJob.label,
   percent: 100,
-  startedAt: 1620000000,
-  updatedAt: 1620001000,
-  id: "job-1",
+  startedAt: exampleJob.startedAt,
+  updatedAt: exampleJob.updatedAt,
+  id: exampleJob.id,
   info: "Processing tiles",
+  url: devData[0].url,
 };
